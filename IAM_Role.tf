d4cfddc53b4e3a00ba3e16a30ce9fe23fcf81b03
Amany gamel <96747879+amanygamel@users.noreply.github.com>
2024-01-31 02:27:56 +0200

    Add files via upload

---
+++ IAM_Role.tf
@@ -0,0 +1,15 @@
+resource "aws_iam_role" "cloudwatch_role" {
+  name = "cloudwatch-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
